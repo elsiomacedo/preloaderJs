a608637c12fc8cce8a85f5445c6251ddd23eb5f0
Elsio Macedo <elsiomacedo@gmail.com>
2023-11-21 13:56:52 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -18,8 +18,8 @@ const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
 svgImage.src = 'https://elsiomacedo.github.io/preloaderJs/yy3.gif';
 svgImage.style.cssText = `
-    max-width: 40px;
-    max-height: 40px;
+    max-width: 80px;
+    max-height: 80px;
     display: none;
     position: absolute;
     top: 50%;
